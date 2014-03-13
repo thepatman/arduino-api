4d80e7359dbe068854ce16fe9f84be131166d29e
chriddyp <christopher.parmer@gmail.com>
2014-03-13 04:19:35 -0400

    2 traces

--- plotly_yun/Arduino/example.ino
+++ plotly_yun/Arduino/example.ino
@@ -4,20 +4,14 @@
 #include <Console.h>
 
 // Initialize plotly "plotters" with your stream tokens.
-// Find your stream tokens in your plotly account here: https://plot.ly/settings .
+// Find your stream tokens in your plotly account here: https://plot.ly/settings
 // Initialize as many plotters as you want! Each plotter will send
 // data to the same plot as a separate line.
-// Make sure that you place these exact stream tokens in your `config.json` file
+// Make sure that you place these exact-same stream tokens in your `config.json` file
 // on the Linino.
 
-//plotly plotter1("put_your_10_character_stream_token_here");
-//plotly plotter2("put_another_token_here");
-//plotly plotter3("and_another!");
-
-plotly plotter1("3ab1qn3z23");
-plotly plotter2("9x4gkihdlx");
-plotly plotter3("xi83ke94lw");
-
+plotly plotter1("put_your_10_character_stream_token_here");
+plotly plotter2("put_another_token_here");
 
 void setup() { 
 
@@ -45,12 +39,10 @@ void loop() {
     // Each "plotter" will send data to the same plot as separate lines
     plotter1.plot( analogRead(A0) );
     plotter2.plot( analogRead(A1) );
-    plotter3.plot( analogRead(A2) );
 
     // You can also set the "x" value manually 
     // by placing a pair of points in the plot function:
 //    plotter1.plot( millis(), analogRead(A0) );
 //    plotter2.plot( millis(), analogRead(A1) );
-//    plotter3.plot( millis(), analogRead(A2) );    
     delay(100);
 }
